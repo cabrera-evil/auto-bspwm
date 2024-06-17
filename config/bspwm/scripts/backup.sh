cffd6372870e5cc01aa229f824df52ce5ac17d60
cabrera-evil <douglascabrera.dev@gmail.com>
2024-06-16 19:40:40 -0500

    refactor: backup script and brightness control

--- config/bspwm/scripts/backup.sh
+++ config/bspwm/scripts/backup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Backup file name
-fdir=~/Documents/Backups
+fdir=$HOME/Documents/Backups
 fname=bk-$(date +%Y-%m-%d).zip
 
-# List of important directories
-dirs='git npm work server Documents Pictures'
+# List of important directories with absolute paths
+dirs="$HOME/git $HOME/npm $HOME/work $HOME/server $HOME/Documents $HOME/Pictures"
 
 # List of excluded files
-excluded="*/node_modules/*" -x "*.zip"
+excluded="*/node_modules/* *.zip"
+
+# Check if directories exist before creating the backup
+for dir in $dirs; do
+    if [ ! -d "$dir" ]; then
+        echo "Directory '$dir' not found."
+        exit 1
+    fi
+done
 
 # Compressing folders but don't include 'node_modules'
-zip -q -r $fdir/$fname $dirs -x $excluded
+zip -q -r "$fdir/$fname" $dirs -x $excluded
