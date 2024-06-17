d95dee480219a984c786aa9d4ce3a3645de9b7fb
cabrera-evil <douglascabrera.dev@gmail.com>
2024-06-17 12:13:18 -0500

    refactor: tmp directories and header messages

--- setup.sh
+++ setup.sh
@@ -64,41 +64,41 @@ if [ "$user" == "root" ]; then
 	exit 1
 else
 	banner
-	header "[*] Installing necessary packages for the environment..."
+	header "Installing necessary packages for the environment..."
 	sudo apt install -y kitty rofi feh xclip ranger i3lock-fancy scrot scrub wmname firejail imagemagick cmatrix htop neofetch python3-pip procps tty-clock fzf lsd bat pamixer flameshot playerctl brightnessctl blueman bluez
 
-	header "[*] Installing pywal..."
+	header "Installing pywal..."
 	sudo pip3 install pywal --break-system
 
-	header "[*] Starting installation of necessary dependencies for the environment..."
+	header "Starting installation of necessary dependencies for the environment..."
 
-	header "[*] Installing necessary dependencies for bspwm..."
+	header "Installing necessary dependencies for bspwm..."
 	sudo apt install -y build-essential git vim libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libuv1-dev
 
-	header "[*] Installing necessary dependencies for polybar..."
+	header "Installing necessary dependencies for polybar..."
 	sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
 
-	header "[*] Installing necessary dependencies for picom..."
+	header "Installing necessary dependencies for picom..."
 	sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
 
-	header "[*] Starting installation of the tools..."
-	mkdir ~/tools && cd ~/tools
+	header "Starting installation of the tools..."
+	cd /tmp
 
-	header "[*] Installing bspwm..."
+	header "Installing bspwm..."
 	git clone https://github.com/baskerville/bspwm.git
 	cd bspwm
 	make -j$(nproc)
 	sudo make install
 	cd ..
 
-	header "[*] Installing sxhkd..."
+	header "Installing sxhkd..."
 	git clone https://github.com/baskerville/sxhkd.git
 	cd sxhkd
 	make -j$(nproc)
 	sudo make install
 	cd ..
 
-	header "[*] Installing polybar..."
+	header "Installing polybar..."
 	git clone --recursive https://github.com/polybar/polybar
 	cd polybar
 	mkdir build
@@ -108,36 +108,36 @@ else
 	sudo make install
 	cd ../../
 
-	header "[*] Installing picom..."
+	header "Installing picom..."
 	git clone https://github.com/ibhagwan/picom.git
 	cd picom
 	git submodule update --init --recursive
 	meson --buildtype=release . build
 	ninja -C build
 	sudo ninja -C build install
-	cd ..
+	cd $dir
 
-	header "[*] Installing Oh My Zsh and Powerlevel10k for user $user..."
+	header "Installing Oh My Zsh and Powerlevel10k for user $user..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-	header "[*] Installing Oh My Zsh and Powerlevel10k for user root..."
+	header "Installing Oh My Zsh and Powerlevel10k for user root..."
 	sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.oh-my-zsh/custom/themes/powerlevel10k
 
-	header "[*] Installing zsh-autosuggestions for user $user..."
+	header "Installing zsh-autosuggestions for user $user..."
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-	header "[*] Installing zsh-autosuggestions for user root..."
+	header "Installing zsh-autosuggestions for user root..."
 	sudo git clone https://github.com/zsh-users/zsh-autosuggestions /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-	header "[*] Installing zsh-syntax-highlighting for user $user..."
+	header "Installing zsh-syntax-highlighting for user $user..."
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-	header "[*] Installing zsh-syntax-highlighting for user root..."
+	header "Installing zsh-syntax-highlighting for user root..."
 	sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-	header "[*] Configuring touchpad..."
+	header "Configuring touchpad..."
 	sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee /etc/X11/xorg.conf.d/90-touchpad.conf <<'EOF' 1>/dev/null
 Section "InputClass"
         Identifier "touchpad"
@@ -150,7 +150,7 @@ Section "InputClass"
 EndSection
 EOF
 
-	header "[*] Configuring fonts..."
+	header "Configuring fonts..."
 	if [[ -d "$fdir" ]]; then
 		cp -rv $dir/fonts/* $fdir
 	else
@@ -158,7 +158,7 @@ EOF
 		cp -rv $dir/fonts/* $fdir
 	fi
 
-	header "[*] Configuring wallpaper..."
+	header "Configuring wallpaper..."
 	if [[ -d "~/Pictures/Wallpapers" ]]; then
 		cp -rv $dir/wallpapers/* ~/Pictures/Wallpapers
 	else
@@ -168,20 +168,20 @@ EOF
 	wal -nqi ~/Pictures/Wallpapers/archkali.png
 	sudo wal -nqi ~/Pictures/Wallpapers/archkali.png
 
-	header "[*] Configuring configuration files..."
+	header "Configuring configuration files..."
 	ln -s $dir/config/* ~/.config/
 
-	header "[*] Configuring the .zshrc and .p10k.zsh files..."
+	header "Configuring the .zshrc and .p10k.zsh files..."
 	cp -v $dir/.zshrc ~/.zshrc
 	sudo ln -sfv ~/.zshrc /root/.zshrc
 	cp -v $dir/.p10k.zsh ~/.p10k.zsh
 	sudo ln -sfv ~/.p10k.zsh /root/.p10k.zsh
 
-	header "[*] Configuring scripts..."
+	header "Configuring scripts..."
 	sudo cp -v $dir/scripts/whichSystem.py /usr/local/bin/
 	touch ~/.config/polybar/scripts/target
 
-	header "[*] Configuring necessary permissions and symbolic links..."
+	header "Configuring necessary permissions and symbolic links..."
 	sudo chmod +x /usr/local/bin/whichSystem.py
 	sudo chmod +x /usr/local/bin/screenshot
 	sudo chmod +x /usr/local/share/zsh/site-functions/_bspc
@@ -190,9 +190,6 @@ EOF
 	sudo touch /root/.config/polybar/scripts/target
 	sudo ln -sfv ~/.config/polybar/scripts/target /root/.config/polybar/scripts/target
 	cd ..
-
-	header "[*] Removing tools directory..."
-	rm -rfv ~/tools
 
 	header "\n${GREEN}[+] Environment configured :D\n${NC}"
 
