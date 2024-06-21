db6097323f060b90687e0c0bd29042c3e67ede08
cabrera-evil <douglascabrera.dev@gmail.com>
2024-06-21 08:27:11 -0600

    chore(update): polybar monitor launch

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,11 +4,7 @@
 
 DIR="$HOME/.config/polybar"
 
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+# Launch polybar for each monitor
+for monitor in $(xrandr -q | awk '/ connected/ {print $1}'); do
+    MONITOR=$monitor polybar -q -c $DIR/config.ini main &
+done
