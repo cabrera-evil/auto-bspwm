78e76c489854efdde0ee309baee0d37d04505456
cabrera-evil <douglascabrera.dev@gmail.com>
2024-08-22 16:58:49 -0600

    fix: update script

--- config/polybar/scripts/updates.sh
+++ config/polybar/scripts/updates.sh
@@ -2,42 +2,43 @@
 
 NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
 
-get_total_updates() { UPDATES=$(~/.config/polybar/scripts/checkupdates 2>/dev/null | wc -l); }
+# Function to get the number of available updates
+get_total_updates() {
+    UPDATES=$(apt list --upgradable 2>/dev/null | grep -c "upgradable")
+}
 
+# Function to send notifications based on the number of updates
+send_notification() {
+    if hash dunstify &>/dev/null; then
+        case $UPDATES in
+        [5-9][0-9] | [1-9][0-9][0-9]*)
+            dunstify -r 2593 -u critical -i $NOTIFY_ICON \
+                "You really need to update!!" "$UPDATES New packages"
+            ;;
+        [2-4][0-9])
+            dunstify -r 2593 -u normal -i $NOTIFY_ICON \
+                "You should update soon" "$UPDATES New packages"
+            ;;
+        [3-9])
+            dunstify -r 2593 -u low -i $NOTIFY_ICON \
+                "You have updates" "$UPDATES New packages"
+            ;;
+        esac
+    fi
+}
+
+# Main loop
 while true; do
     get_total_updates
+    send_notification
 
-    # notify user of updates
-    if hash notify-send &>/dev/null; then
-        if (( UPDATES > 50 )); then
-            notify-send -u critical -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
-        elif (( UPDATES > 25 )); then
-            notify-send -u normal -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
-        elif (( UPDATES > 2 )); then
-            notify-send -u low -i $NOTIFY_ICON \
-                "$UPDATES New packages"
-        fi
-    fi
-
-    # when there are updates available
-    # every 10 seconds another check for updates is done
-    while (( UPDATES > 0 )); do
-        if (( UPDATES == 1 )); then
-            echo " $UPDATES"
-        elif (( UPDATES > 1 )); then
-            echo " $UPDATES"
-        else
-            echo " None"
-        fi
+    while ((UPDATES > 0)); do
+        echo " $UPDATES"
         sleep 10
         get_total_updates
     done
 
-    # when no updates are available, use a longer loop, this saves on CPU
-    # and network uptime, only checking once every 30 min for new updates
-    while (( UPDATES == 0 )); do
+    while ((UPDATES == 0)); do
         echo " None"
         sleep 1800
         get_total_updates
