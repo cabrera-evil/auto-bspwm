dee62990c342c4cfb9364bde4135376bcf04c511
cabrera-evil <douglascabrera.dev@gmail.com>
2024-08-22 18:18:26 -0600

    chore(update): dynamic network interface

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,21 +3,26 @@
 # Set global variables
 DIR="$HOME/.config/polybar"
 XRANDR_CACHE=""
+NETWORK_CACHE=""
 
 # Function to launch Polybar on each connected monitor
 launch_polybar() {
     pkill -x polybar
     for monitor in $(xrandr -q | awk '/ connected/ {print $1}'); do
-        MONITOR=$monitor polybar -q -c "$DIR/config.ini" main &
+        export MONITOR=$monitor
+        export NETWORK_INTERFACE=$(nmcli -t -f active,device dev wifi | egrep '^yes' | cut -d':' -f2)
+        polybar -q -c "$DIR/config.ini" main &
     done
 }
 
-# Monitor setup watcher to automatically refresh Polybar when monitors change
+# Monitor setup watcher to automatically refresh Polybar when monitors or network change
 while true; do
     XRANDR_CURRENT=$(xrandr --listmonitors)
-    if [ "$XRANDR_CURRENT" != "$XRANDR_CACHE" ]; then
+    NETWORK_CURRENT=$(nmcli -t -f active,device dev wifi | egrep '^yes' | cut -d':' -f2)
+    if [ "$XRANDR_CURRENT" != "$XRANDR_CACHE" ] || [ "$NETWORK_CURRENT" != "$NETWORK_CACHE" ]; then
         launch_polybar
         XRANDR_CACHE=$XRANDR_CURRENT
+        NETWORK_CACHE=$NETWORK_CURRENT
     fi
     sleep 5
 done
