ac69d51c22bff2dc12eaac237c7edcc0874b8725
cabrera-evil <douglascabrera.dev@gmail.com>
2025-04-13 11:50:25 -0600

    chore(update): nvim auto session

--- config/nvim/lua/configs/auto-session.lua
+++ config/nvim/lua/configs/auto-session.lua
@@ -2,7 +2,7 @@ local auto_session = require("auto-session")
 
 auto_session.setup({
   log_level = "info",
-  auto_session_suppress_dirs = { "~/", "~/projects", "~/Downloads" },
+  auto_session_suppress_dirs = { "~/" },
   auto_session_enable_last_session = true,
   auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
   auto_session_use_git_branch = true,
