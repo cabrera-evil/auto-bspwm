ba79917486653408d76b9da28cd6a40915db77b3
cabrera-evil <douglascabrera.dev@gmail.com>
2025-04-13 12:02:27 -0600

    refactor: nchad version

--- config/nvim/lua/configs/conform.lua
+++ config/nvim/lua/configs/conform.lua
@@ -1,17 +1,17 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = {"stylua"},
+        javascript = {"prettier"},
+        typescript = {"prettier"}
+    },
 
-  -- Use Prettier as a fallback if no formatter is defined for the filetype
-  fallback_formatter = "prettier",
+    -- Use Prettier as a fallback if no formatter is defined for the filetype
+    fallback_formatter = "prettier",
 
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true
+    }
 }
 
 return options
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,36 +1,36 @@
-return { {
-  "stevearc/conform.nvim",
-  event = "BufWritePre",
-  opts = require("configs.conform")
+return {{
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = require("configs.conform")
 }, {
-  "neovim/nvim-lspconfig",
-  config = function()
-    require("configs.lspconfig")
-  end
+    "neovim/nvim-lspconfig",
+    config = function()
+        require("configs.lspconfig")
+    end
 }, {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  opts = require("configs.treesitter")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = require("configs.treesitter")
 }, {
-  "williamboman/mason-lspconfig.nvim",
-  lazy = false,
-  opts = require("configs.mason-lspconfig")
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = require("configs.mason-lspconfig")
 }, {
-  "github/copilot.vim",
-  lazy = false
+    "github/copilot.vim",
+    lazy = false
 }, {
-  "rmagatti/auto-session",
-  config = function()
-    require("configs.auto-session")
-  end
+    "rmagatti/auto-session",
+    config = function()
+        require("configs.auto-session")
+    end
 }, {
-  "kdheepak/lazygit.nvim",
-  lazy = true,
-  cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = { {
-    "<leader>lg",
-    "<cmd>LazyGit<cr>",
-    desc = "LazyGit"
-  } }
-} }
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"},
+    dependencies = {"nvim-lua/plenary.nvim"},
+    keys = {{
+        "<leader>lg",
+        "<cmd>LazyGit<cr>",
+        desc = "LazyGit"
+    }}
+}}
