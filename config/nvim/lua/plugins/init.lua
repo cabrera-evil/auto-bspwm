50517cd85bedd8d148e52a07ec2a56f8aee3b047
cabrera-evil <douglascabrera.dev@gmail.com>
2025-04-13 10:57:55 -0600

    feat: lazygit integration and lsp update

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,40 +1,34 @@
 return { {
   "stevearc/conform.nvim",
-  event = 'BufWritePre',
-  opts = require "configs.conform"
+  event = "BufWritePre",
+  opts = require("configs.conform")
 }, {
   "neovim/nvim-lspconfig",
   config = function()
-    require "configs.lspconfig"
+    require("configs.lspconfig")
   end
 }, {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   opts = {
-    ensure_installed = { "vim", "lua", "vimdoc", "html", "css" }
+    ensure_installed = { "vim", "lua", "vimdoc", "html", "css", "javascript", "typescript", "tsx", "json",
+      "markdown", "dockerfile", "yaml", "bash" },
+    highlight = {
+      enable = true
+    },
+    indent = {
+      enable = true
+    }
   }
 }, {
   "williamboman/mason-lspconfig.nvim",
   lazy = false,
   opts = {
     auto_install = true,
-    ensure_installed = {
-      "bashls",
-      "cssls",
-      "docker_compose_language_service",
-      "dockerls",
-      "eslint",
-      "graphql",
-      "html",
-      "jsonls",
-      "lua_ls",
-      "marksman",
-      "prismals",
-      "pyright",
-      "tailwindcss",
-      "terraformls",
-      "vimls",
-      "yamlls"
-    }
+    ensure_installed = { "html", "cssls", "tailwindcss", "eslint", "jsonls", "graphql", "emmet_ls",
+      "lua_ls", "yamlls", "ansiblels", "terraformls", "tflint",
+      "dockerls", "docker_compose_language_service", "bashls", "vimls", "marksman", "lemminx",
+      "pyright", "prismals", "sqlls" }
   }
 }, {
   "github/copilot.vim",
@@ -42,6 +36,19 @@ return { {
 }, {
   "rmagatti/auto-session",
   config = function()
-    require("auto-session").setup()
+    require("auto-session").setup({
+      log_level = "info",
+      auto_session_suppress_dirs = { "~" }
+    })
   end
+}, {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = { {
+    "<leader>lg",
+    "<cmd>LazyGit<cr>",
+    desc = "LazyGit"
+  } }
 } }
