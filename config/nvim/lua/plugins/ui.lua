d83dbfa4d0940c670757db2b27a451b80a6a35fc
cabrera-evil <douglascabrera.dev@gmail.com>
2025-05-23 22:26:24 -0600

    feat: oil commands

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,4 @@
-return {{
+return { {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
@@ -7,38 +7,38 @@ return {{
         require("configs.catppuccin")
     end
 },
--- {
---     "folke/snacks.nvim",
---     dependencies = {"echasnovski/mini.icons"},
---     priority = 1000,
---     lazy = false,
---     opts = require("configs.snaks")
--- },
-{
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	opts = function(_, opts)
-		-- Other blankline configuration here
-		return require("indent-rainbowline").make_opts(opts)
-	end,
-	dependencies = {
-		"TheGLander/indent-rainbowline.nvim",
-	},
-},
-{
-    "stevearc/oil.nvim",
-    lazy = false,
-    dependencies = {{
-        "echasnovski/mini.icons",
-        lazy = false
-    }, {"nvim-tree/nvim-web-devicons"}},
-    config = function()
-        local oil = require("oil")
-        oil.setup()
-        vim.keymap.set("n", "-", oil.toggle_float, {})
-    end
-}, {
+    -- {
+    --     "folke/snacks.nvim",
+    --     dependencies = {"echasnovski/mini.icons"},
+    --     priority = 1000,
+    --     lazy = false,
+    --     opts = require("configs.snaks")
+    -- },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = function(_, opts)
+            -- Other blankline configuration here
+            return require("indent-rainbowline").make_opts(opts)
+        end,
+        dependencies = {
+            "TheGLander/indent-rainbowline.nvim",
+        },
+    },
+    {
+        "stevearc/oil.nvim",
+        lazy = false,
+        dependencies = { {
+            "echasnovski/mini.icons",
+            lazy = false
+        }, { "nvim-tree/nvim-web-devicons" } },
+        config = function()
+            local oil = require("oil")
+            oil.setup()
+            vim.keymap.set("n", "-", oil.toggle_float, {})
+        end
+    }, {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
-    dependencies = {"nvim-tree/nvim-web-devicons"}
-}}
+    dependencies = { "nvim-tree/nvim-web-devicons" }
+} }
