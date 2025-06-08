0cbf3db0f579c79eff152c970c7b40fdc9c36e27
cabrera-evil <douglascabrera.dev@gmail.com>
2025-06-07 23:53:18 -0600

    feat: nvim liveshare

---
+++ config/nvim/lua/plugins/live-share.lua
@@ -0,0 +1,13 @@
+return {
+  "azratul/live-share.nvim",
+  dependencies = {
+    "jbyuki/instant.nvim",
+  },
+  config = function()
+    vim.g.instant_username = "cabrera-evil"
+    require("live-share").setup({
+      max_attempts = 40, -- Maximum number of attempts to read the URL from service(serveo.net or localhost.run), every 250 ms
+      service = "serveo.net", -- Service to use, options are serveo.net or localhost.run
+    })
+  end,
+}
