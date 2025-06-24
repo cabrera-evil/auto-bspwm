2f1b08b864ab45fa1619d49200db766fb36556e0
cabrera-evil <douglascabrera.dev@gmail.com>
2025-06-23 23:25:21 -0600

    refactor: dos2unix script for current buffer

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,5 +7,19 @@ vim.keymap.set("n", "<leader>go", function()
 end, { desc = "Opens remote repository" })
 
 vim.keymap.set("n", "<leader>cp", function()
-  vim.fn.jobstart("find . -type f -exec dos2unix {} +", { detach = true })
-end, { desc = "Convert files to Unix format" })
+  local file = vim.api.nvim_buf_get_name(0)
+  vim.fn.jobstart({ "dos2unix", file }, {
+    detach = true,
+    on_exit = function(_, code)
+      if code == 0 then
+        vim.schedule(function()
+          vim.notify("Converted to Unix format: " .. file, vim.log.levels.INFO)
+        end)
+      else
+        vim.schedule(function()
+          vim.notify("Failed to convert: " .. file, vim.log.levels.ERROR)
+        end)
+      end
+    end,
+  })
+end, { desc = "Convert current file to Unix format (dos2unix)" })
