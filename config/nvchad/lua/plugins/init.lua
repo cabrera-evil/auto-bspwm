be5fa87aa40355d81c3ba995f8b07cceb6ef39fd
cabrera-evil <douglascabrera.dev@gmail.com>
2025-06-23 21:21:47 -0600

    chore(delete): nvchad conf

--- config/nvchad/lua/plugins/init.lua
+++
@@ -1,2 +0,0 @@
-return vim.tbl_deep_extend("force", require("plugins.core"), require("plugins.lsp"), require("plugins.ui"),
-    require("plugins.tools"), require("plugins.coding"), require("plugins.git"), require("plugins.testing"))
