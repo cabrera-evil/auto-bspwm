6d785773222b3dc4926a3da0a2673dcd5a43ba55
cabrera-evil <douglascabrera.dev@gmail.com>
2025-06-23 23:28:09 -0600

    feat: dynamic live-share username

--- config/nvim/lua/plugins/live-share.lua
+++ config/nvim/lua/plugins/live-share.lua
@@ -4,7 +4,7 @@ return {
     "jbyuki/instant.nvim",
   },
   config = function()
-    vim.g.instant_username = "cabrera-evil"
+    vim.g.instant_username = os.getenv("USER") or os.getenv("USERNAME") or "guest"
     require("live-share").setup({
       max_attempts = 40, -- Maximum number of attempts to read the URL from service(serveo.net or localhost.run), every 250 ms
       service = "serveo.net", -- Service to use, options are serveo.net or localhost.run
