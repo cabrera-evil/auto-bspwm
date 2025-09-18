05e91c74be542d6e29b2f6842864b12c6b6087b7
Douglas Cabrera <douglascabrera.dev@gmail.com>
2025-09-18 16:49:26 -0600

    refactor: extend existing lsp config

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -16,32 +16,12 @@ local configs = require("lspconfig/configs")
 
 -- GitHub Actions Language Server
 lspconfig.gh_actions_ls.setup({
-  cmd = { "gh-actions-language-server", "--stdio" },
   filetypes = { "yaml", "yml" },
   root_dir = lspconfig.util.root_pattern(".github/workflows", ".github"),
 })
 
--- GolangCI-Lint Language Server
-if not configs.golangcilsp then
-  configs.golangcilsp = {
-    default_config = {
-      cmd = { "golangci-lint-langserver" },
-      root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
-      init_options = {
-        command = {
-          "golangci-lint",
-          "run",
-          "--output.json.path",
-          "stdout",
-          "--show-stats=false",
-          "--issues-exit-code=1",
-        },
-      },
-    },
-  }
-end
-
 -- Go Linter Language Server
 lspconfig.golangci_lint_ls.setup({
   filetypes = { "go", "gomod" },
+  root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
 })
